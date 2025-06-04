0a209c26dbfddc8ff01a8e86f0e6269b4e81dcb1
jaimebp07 <andrescodigo07@gmail.com>
2025-06-04 16:21:00 -0500

    add readme

--- backend/Infrastructure/Soap/SoapClient.cs
+++ backend/Infrastructure/Soap/SoapClient.cs
@@ -4,8 +4,11 @@ namespace Infrastructure.Soap
     {
         public string GetSoapResponse()
         {
-            // Dummy XML response for testing
-            return @"<Client><CustomerName>Pepito Perez</CustomerName><TotalBalance>1234567890123456</TotalBalance><AccountStatus>Active</AccountStatus></Client>";
+            // Dummy XML invalid balance
+            //return @"<Client><CustomerName>Pepito Perez</CustomerName><TotalBalance>1234567890123456</TotalBalance><AccountStatus>Active</AccountStatus></Client>";
+            // Dummy XML valid balance
+            return @"<Client><CustomerName>Pepito Perez</CustomerName><TotalBalance>123456789</TotalBalance><AccountStatus>Active</AccountStatus></Client>";
+            
         }
     }
 }
